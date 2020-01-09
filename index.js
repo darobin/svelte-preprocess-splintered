8469519272c13a87c4e5dd51538940aaaa29fb34
Robin Berjon <robin@berjon.com>
2020-01-08 23:24:47 -0500

    add dependencies support

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ module.exports = function splintered () {
         , bn = basename(filename, extname(filename))
       ;
       return new Promise((resolve, reject) => {
-        makeFileReader(dir, bn, 'mjs', 'script context="module"', (err, mod) => {
+        makeFileReader(dir, bn, 'mjs', 'script context="module"', (err, mod, modDep) => {
           if (err) return reject(err);
-          makeFileReader(dir, bn, 'js', 'script', (err, js) => {
+          makeFileReader(dir, bn, 'js', 'script', (err, js, jsDep) => {
             if (err) return reject(err);
-            makeFileReader(dir, bn, 'css', 'style', (err, css) => {
+            makeFileReader(dir, bn, 'css', 'style', (err, css, cssDep) => {
               if (err) return reject(err);
-              resolve({ code: `\n${[mod, js, css].filter(Boolean).join('')}${content}` });
+              let dependencies = [modDep, jsDep, cssDep].filter(Boolean);
+              // XXX:
+              //  - check that this is correct with docs
+              //  - include in tests
+              resolve({ code: `\n${[mod, js, css].filter(Boolean).join('')}${content}`, dependencies });
             });
           });
         });
@@ -26,8 +30,9 @@ module.exports = function splintered () {
 };
 
 function makeFileReader (directory, base, extension, elName, cb) {
-  readFile(join(directory, `${base}.${extension}`), 'utf8', (err, data) => {
+  let file = join(directory, `${base}.${extension}`);
+  readFile(file, 'utf8', (err, data) => {
     if (err) return cb(null); // if we can't read it's actually fine
-    cb(null, `<${elName}>\n${data}</${elName.replace(/\s.*/, '')}>\n\n`);
+    cb(null, `<${elName}>\n${data}</${elName.replace(/\s.*/, '')}>\n\n`, file);
   });
 }
